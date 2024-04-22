ae41f286fce0d658a722c7975bf485fbade1639d
SrGonao <goncalo.paulo@uniroma1.it>
2024-04-22 09:01:46 +0000

    Added simple tokenizer and function to test tokenization. Skips strange artifacts

--- dataset.py
+++ dataset.py
@@ -1,28 +1,73 @@
 from datasets import load_dataset
-from tokenizer import CheessTokenizer
 from torch import torch
 import numpy as np
 from tqdm import tqdm
-def process(example,tokenizer):
+from tokenizer import ChessSimpleTokenizer,ChessTokenizer
+
+def tokenize(example,tokenizer):
         column_name = "transcript"
         split_text = example[column_name].split(";")
         #print(split_text)
         first = split_text[1]
-        ids = np.array([tokenizer.encode(first)])
+        ids = tokenizer.encode(first)   
+        decode = tokenizer.decode(ids)
+        if len(decode)>len(first):
+            decode = decode[:-1]
+        try:
+            assert first == decode
+        except:
+            print("Skipped")
+            print(first,decode)
+            print("############")
+            ids = []
         out = {"ids": ids, "len": len(ids)}
         return out
-def pretokenize_dataset(dataset_name,file_name,tokenizer):
+
+def decode(example,tokenizer):
+    column_name = "train"
+    ids = example[column_name]["ids"]
+    return tokenizer.decode(ids)
+
+def pretokenize_dataset(dataset_name,file_name,tokenizer,name="simple"):
     num_processes = 64
     original_dataset = load_dataset(dataset_name, data_files=file_name)["train"]
     split = original_dataset.train_test_split(test_size=0.2,seed=42)
-    tokenized = split.map(process,fn_kwargs={"tokenizer":tokenizer},remove_columns="transcript",num_proc=num_processes)
+    tokenized = split.map(tokenize,fn_kwargs={"tokenizer":tokenizer},remove_columns="transcript",num_proc=num_processes)
     train = tokenized["train"]
-    train.save_to_disk("train_dataset"+"_"+file_name)
+
+    train.save_to_disk(f"datasets/{name}_train_dataset"+"_"+file_name)
     test = tokenized["test"]
-    test.save_to_disk("test_dataset"+"_"+file_name)    
+    test.save_to_disk(f"datasets/{name}_test_dataset"+"_"+file_name)    
     #return tokenized
+
+def test_tokenization(dataset_name,file_name,tokenizer):
+    dataset = load_dataset(dataset_name, data_files=file_name)
+    dataset = dataset["train"]
+    examples = 0
+    for example in dataset:
+        split_text = example["transcript"].split(";")
+        first = split_text[1]
+        ids = tokenizer.encode(first)
+        decoded = tokenizer.decode(ids)
+        if len(decoded)>len(first):
+            decoded = decoded[:-1]
+
+        try: 
+             assert first == decoded
+        except:    
+            print(f"##################### {examples} ######################")
+            print(first)
+            print("\n")
+            print(decoded)
+            exit()
+        examples+=1
 
 
 dataset_name="adamkarvonen/chess_games"
 file_name="stockfish_dataset_blocks.zip"
-pretokenize_dataset(dataset_name,file_name,CheessTokenizer())
+simple_tokenizer = ChessSimpleTokenizer()
+normal_tokenizer = ChessTokenizer()
+
+#test_tokenization(dataset_name,file_name,normal_tokenizer)
+pretokenize_dataset(dataset_name,file_name,simple_tokenizer,name="simple")
+pretokenize_dataset(dataset_name,file_name,normal_tokenizer,name="normal")
